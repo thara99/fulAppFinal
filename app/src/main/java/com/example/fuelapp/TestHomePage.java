f6198a63e7a28611ecb992872c92cca8265797e1
IT19132624_T.A.H.Saranga <it19132624@my.sliit.lk>
2022-10-16 15:23:27 +0530

    new files hasitha

---
+++ app/src/main/java/com/example/fuelapp/TestHomePage.java
@@ -0,0 +1,14 @@
+package com.example.fuelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_home_page);
+    }
+}
